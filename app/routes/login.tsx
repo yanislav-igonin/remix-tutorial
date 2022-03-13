fdc31d41122e8eca4f51b03540c5388864c88abd
Yanislav Igonin <yanislavdabest@gmail.com>
2022-03-13 23:32:24 +0200

    add login function

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -7,6 +7,7 @@ import {
 } from "remix";
 
 import { db } from "~/utils/db.server";
+import { login } from "~/utils/session.server";
 import stylesUrl from "~/styles/login.css";
 
 export const links: LinksFunction = () => {
@@ -70,8 +71,14 @@ export const action: ActionFunction = async ({
 
   switch (loginType) {
     case "login": {
-      // login to get the user
-      // if there's no user, return the fields and a formError
+      const user = await login({ username, password });
+      console.log(user);
+      if (!user) {
+        return badRequest({
+          fields,
+          formError: `Username/Password combination is incorrect`,
+        });
+      }
       // if there is a user, create their session and redirect to /jokes
       return badRequest({
         fields,
