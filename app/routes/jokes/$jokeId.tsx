915bb83522b41fed04bc47ab0803b374729fc154
Yanislav Igonin <yanislavdabest@gmail.com>
2022-03-17 09:36:27 +0200

    add catchers for user errors

--- app/routes/jokes/$jokeId.tsx
+++ app/routes/jokes/$jokeId.tsx
@@ -1,5 +1,11 @@
 import type { LoaderFunction } from "remix";
-import { json, Link, useLoaderData, useParams } from "remix";
+import {
+  json,
+  Link,
+  useLoaderData,
+  useCatch,
+  useParams,
+} from "remix";
 import type { Joke } from "@prisma/client";
 
 import { db } from "~/utils/db.server";
@@ -12,7 +18,11 @@ export const loader: LoaderFunction = async ({
   const joke = await db.joke.findUnique({
     where: { id: params.jokeId },
   });
-  if (!joke) throw new Error("Joke not found");
+  if (!joke) {
+    throw new Response("What a joke! Not found.", {
+      status: 404,
+    });
+  }
   const data: LoaderData = { joke };
   return json(data);
 };
@@ -27,6 +37,19 @@ export default function JokeRoute() {
       <Link to=".">{data.joke.name} Permalink</Link>
     </div>
   );
+}
+
+export function CatchBoundary() {
+  const caught = useCatch();
+  const params = useParams();
+  if (caught.status === 404) {
+    return (
+      <div className="error-container">
+        Huh? What the heck is "{params.jokeId}"?
+      </div>
+    );
+  }
+  throw new Error(`Unhandled error: ${caught.status}`);
 }
 
 export function ErrorBoundary() {
